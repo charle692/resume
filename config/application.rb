1896daefe094ec104f174819444e922680dda21a
Ryan O'Connor <charle692@gmail.com>
2014-11-06 14:18:35 -0500

    Fixed bug in application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,15 +9,9 @@ require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-# Enable the asset pipeline
-config.assets.enabled = true
-
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 module Resume
   class Application < Rails::Application
