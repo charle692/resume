628798ab24de993aec09622eb313580d2eb4f317
Ryan O'Connor <charle692@gmail.com>
2014-12-20 23:50:15 -0500

    Removed routes and changed env settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'about' => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
+  root 'static_pages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
