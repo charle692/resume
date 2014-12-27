122426084d5a5fa94d981f20eaa3df9fc7662388
Ryan O'Connor <charle692@gmail.com>
2014-12-26 20:28:25 -0500

    added page navigation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,24 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+var ready;
+
+ready = function(){
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
